df2c87625ea7b6d5509f942731950f38eac90f71
Maksim Krylosov <aequiternus@gmail.com>
2014-12-09 23:06:19 +0300

    init

---
+++ Data.js
@@ -0,0 +1,202 @@
+
+module.exports = RedisData;
+
+var Data = require('rtcs.io-storage').Data;
+var redis = require('redis');
+var base64id = require('base64id');
+
+function RedisData(options) {
+    Data.call(this, options);
+
+    if ('undefined' == typeof options.prefix) {
+        this.options.prefix = 'rtcs.io:'
+    }
+    if ('undefined' == typeof options.prefixSession) {
+        this.options.prefixSession = 'session:'
+    }
+    if ('undefined' == typeof options.prefixUser) {
+        this.options.prefixUser = 'user:'
+    }
+    if ('undefined' == typeof options.prefixRoom) {
+        this.options.prefixRoom = 'room:'
+    }
+    if ('undefined' == typeof options.prefixLog) {
+        this.options.prefixLog = 'log:'
+    }
+
+    this.options.prefixSession = this.options.prefix + this.options.prefixSession;
+    this.options.prefixUser = this.options.prefix + this.options.prefixUser;
+    this.options.prefixRoom = this.options.prefix + this.options.prefixRoom;
+    this.options.prefixLog = this.options.prefix + this.options.prefixLog;
+
+    if (options.client) {
+        this.redis = options.client;
+    } else {
+        var args = [];
+        if (options.socket) {
+            args.push(options.socket);
+        } else if (options.port && options.host) {
+            args.push(options.port);
+            args.push(options.host);
+        }
+        args.push(options);
+
+        this.redis = redis.createClient.apply(null, args);
+    }
+
+    var self = this;
+    self.redis.on('error', function (err) {
+        self.emit('error', err);
+    });
+}
+
+RedisData.prototype.__proto__ = Data;
+
+RedisData.prototype.close = function() {
+    if (this.redis.connected) {
+        this.redis.quit();
+    }
+};
+
+RedisData.prototype.getSession = function(sessionId, callback) {
+    this.redis.get(this.options.prefixSession + sessionId, callback);
+};
+
+RedisData.prototype.getUser = function(userId, callback) {
+    this.redis.get(this.options.prefixUser + userId, function(err, reply) {
+        callback(err, err ? null : JSON.parse(reply));
+    });
+};
+
+RedisData.prototype.setUser = function(userId, data, callback) {
+    this.redis.set(this.options.prefixUser + userId, JSON.stringify(data), callback);
+};
+
+RedisData.prototype.getUsers = function(userIds, callback) {
+    this.redis.mget(userIds, function(err, res) {
+        if (err) {
+            callback(err);
+        } else {
+            var users = {};
+            userIds.forEach(function(userId, i) {
+                users[userId] = res[i] && JSON.parse(res[i]);
+            });
+            callback(null, users);
+        }
+    });
+};
+
+RedisData.prototype.getGuest = function(name, callback) {
+    var self = this;
+    if (name) {
+        name = name.trim();
+    }
+    if (name) {
+        self.tryStoreGuest_(name, callback);
+    } else {
+        self.redis.incr(this.prefix + 'guestNum', function(err, name) {
+            if (err) {
+                callback(err);
+            } else {
+                self.tryStoreGuest_(name, callback);
+            }
+        });
+    }
+};
+
+RedisData.prototype.tryStoreGuest_ = function(name, callback) {
+    var self = this;
+    var uid = 'guest:' + base64id.generateId();
+    self.redis.setnx(this.options.prefixUser + uid, JSON.stringify({
+        id: uid,
+        public: {
+            guest: true,
+            name: self.options.guestName + name
+        },
+        rooms: self.options.guestRooms
+    }), function(err, reply) {
+        if (err) {
+            callback(err);
+        } else if (reply) {
+            callback(null, uid);
+        } else {
+            self.tryStoreGuest_(name, callback);
+        }
+    });
+};
+
+Data.prototype.removeGuest = function(userId, callback) {
+    if (userId.match(/guest:/)) {
+        this.redis.del(this.options.prefixUser + userId, callback);
+    } else {
+        process.nextTick(callback.bind(null, null));
+    }
+};
+
+RedisData.prototype.getRoom = function(roomId, callback) {
+    this.redis.get(this.options.prefixRoom + roomId, function(err, reply) {
+        callback(err, err ? null : JSON.parse(reply));
+    });
+};
+
+RedisData.prototype.setRoom = function(roomId, data, callback) {
+    this.redis.set(this.options.prefixRoom + roomId, JSON.stringify(data), callback);
+};
+
+RedisData.prototype.addLog = function(roomId, msg, callback) {
+    var pending = 1;
+
+    if (this.options.historyLength) {
+        ++pending;
+        this.redis.ltrim(this.options.prefixLog + roomId, 1 - this.options.historyLength, -1, done);
+    }
+
+    this.redis.rpush(this.options.prefixLog + roomId, JSON.stringify(msg), done);
+
+    if (this.options.historyExpire) {
+        ++pending;
+        this.redis.pexpire(this.options.prefixLog + roomId, this.options.historyExpire, done);
+    }
+
+    function done(err) {
+        if (err) {
+            callback(err);
+        } else if (!--pending) {
+            callback();
+        }
+    }
+};
+
+RedisData.prototype.getLog = function(roomId, time, callback) {
+    this.redis.lrange(this.options.prefixLog + roomId, 0, -1, function(err, reply) {
+        if (err) {
+            callback(err);
+        } else {
+            var msglog = [];
+            for (var i = 0, l = reply.length; i < l; i++) {
+                if (time && reply[i].time > time) {
+                    msglog.push(JSON.parse(reply[i]));
+                }
+            }
+            callback(null, msglog);
+        }
+    });
+};
+
+Data.prototype.canJoin = function(socket, msg, callback) {
+
+    process.nextTick(callback.bind(null, null));
+};
+
+Data.prototype.canChat = function(socket, msg, callback) {
+    if (msg.message) {
+        var message = msg.message.trim();
+        if (message) {
+            this.canJoin(socket, msg, callback);
+        }
+    }
+};
+
+Data.prototype.canPeer = function(socket, msg, callback) {
+    process.nextTick(callback.bind(null, null));
+};
---
+++ Temp.js
@@ -0,0 +1,173 @@
+
+module.exports = RedisTemp;
+
+var Temp = require('rtcs.io-storage').Temp;
+var redis = require('redis');
+var base64id = require('base64id');
+
+function RedisTemp(options) {
+    Temp.call(this, options);
+
+    if ('undefined' == typeof options.prefix) {
+        this.options.prefix = 'rtcs.io:'
+    }
+    if ('undefined' == typeof options.prefixUserSockets) {
+        this.options.prefixUserSockets = 'userSockets:'
+    }
+    if ('undefined' == typeof options.prefixUserRooms) {
+        this.options.prefixUserRooms = 'userRooms:'
+    }
+    if ('undefined' == typeof options.prefixRoomUsers) {
+        this.options.prefixRoomUsers = 'roomUsers:'
+    }
+    if ('undefined' == typeof options.prefixToken) {
+        this.options.prefixToken = 'token:'
+    }
+
+    this.options.prefixUserSockets = this.options.prefix + this.options.prefixUserSockets;
+    this.options.prefixUserRooms = this.options.prefix + this.options.prefixUserRooms;
+    this.options.prefixRoomUsers = this.options.prefix + this.options.prefixRoomUsers;
+    this.options.prefixToken = this.options.prefix + this.options.prefixToken;
+
+    if (options.client) {
+        this.redis = options.client;
+    } else {
+        var args = [];
+        if (options.socket) {
+            args.push(options.socket);
+        } else if (options.port && options.host) {
+            args.push(options.port);
+            args.push(options.host);
+        }
+        args.push(options);
+
+        this.redis = redis.createClient.apply(null, args);
+    }
+
+    var self = this;
+    self.redis.on('error', function (err) {
+        self.emit('error', err);
+    });
+}
+
+RedisTemp.prototype.__proto__ = Temp;
+
+RedisTemp.prototype.addSocket = function(socketId, userId, callback) {
+    this.redis.sadd(this.options.prefixUserSockets + userId, socketId, callback);
+};
+
+RedisTemp.prototype.removeSocket = function(socketId, userId, callback) {
+    var self = this;
+    self.redis.srem(self.options.prefixUserSockets + userId, socketId, function(err) {
+        if (err) {
+            if (callback) callback(err);
+        } else {
+            self.redis.srandmember(self.options.prefixUserSockets + userId, function(err, reply) {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, !!reply);
+                }
+            });
+        }
+    });
+};
+
+RedisTemp.prototype.getSockets = function(userId, callback) {
+    this.redis.smembers(this.options.prefixUserSockets + userId, callback);
+};
+
+RedisTemp.prototype.addUserToRoom = function(userId, roomId, callback) {
+    var already;
+    var pending = 2;
+
+    this.redis.sadd(this.options.prefixUserRooms + userId, roomId, function(err, reply) {
+        if (!err) {
+            already = !reply;
+        }
+        done(err);
+    });
+
+    this.redis.sadd(this.options.prefixRoomUsers + roomId, userId, done);
+
+    function done(err) {
+        if (err) {
+            if (callback) callback(err);
+        } else if (!--pending) {
+            if (callback) callback(null, already);
+        }
+    }
+};
+
+RedisTemp.prototype.removeUserFromRoom = function(userId, roomId, callback) {
+    var self = this;
+    var already;
+    var has;
+    var pending = 2;
+
+    self.redis.srem(self.options.prefixUserRooms + userId, roomId, done);
+
+    self.redis.srem(self.options.prefixRoomUsers + roomId, userId, function(err, reply) {
+        if (err) {
+            done(err);
+        } else {
+            already = !reply;
+            self.redis.srandmember(self.options.prefixRoomUsers + roomId, function(err, reply) {
+                if (!err) {
+                    has = !!reply;
+                }
+                done(err);
+            });
+        }
+    });
+
+    function done(err) {
+        if (err) {
+            if (callback) callback(err);
+        } else if (!--pending) {
+            if (callback) callback(null, already, has);
+        }
+    }
+};
+
+RedisTemp.prototype.getUserRooms = function(userId, callback) {
+    this.redis.smembers(this.options.prefixUserRooms + userId, callback);
+};
+
+RedisTemp.prototype.getRoomUsers = function(roomId, callback) {
+    this.redis.smembers(this.options.prefixRoomUsers + roomId, callback);
+};
+
+RedisTemp.prototype.createToken = function(data, callback) {
+    var self = this;
+    var token = base64id.generateId();
+    self.redis.setnx(self.options.prefixToken + token, JSON.stringify(data), function(err, reply) {
+        if (err) {
+            callback(err);
+        } else if (reply) {
+            self.redis.pexpire(self.options.prefixToken + token, self.options.tokenExpire, function(err) {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, token);
+                }
+            });
+        } else {
+            self.createToken(data, callback);
+        }
+    });
+};
+
+RedisTemp.prototype.releaseToken = function(token, callback) {
+    var self = this;
+    self.redis.get(self.options.prefixToken + token, function(err, data) {
+        if (err) {
+            callback(err);
+        } else if (data) {
+            callback(null, JSON.parse(data));
+            self.redis.del(self.options.prefixToken + token);
+        } else {
+            callback();
+        }
+    });
+};
